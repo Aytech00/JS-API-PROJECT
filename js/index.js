5f880929d72a8389b40ab1c6059482d998547254
Aytech <ayokunumitoluwani00@gmail.com>
2022-08-30 00:03:14 +0100

    new commit

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ document.querySelector('#btn').addEventListener('click', ()=>{
 // Nav toggle
 
  const items = document.getElementById('menu-items');
- caches
+
 
  document.querySelector('#icon').addEventListener('click', ()=>{
 
